cf4955e7a511482dce8194eb80c12ecbdc2df7c7
Krzysztof Ilnicki <pohlaniacz@gmail.com>
2024-03-10 23:24:46 +0100

    data from file

--- src/App.js
+++ src/App.js
@@ -2,18 +2,33 @@ import './styles/App.css';
 import Header from "./components/editor/Header";
 import Slider from "./components/editor/Slider";
 import ActionButton from "./components/layout/ActionButton";
+import data from "./data"
 
 function App() {
+
+    const blocks = data.map(item => {
+        if (item.type === "header") {
+            return <Header
+                key={item.id}
+                position={item.position}
+                text={item.data.title}
+            >
+                <ActionButton text="up"/>
+                <ActionButton text="down" />
+            </Header>
+        }
+        if (item.type === "slider") {
+            return <Slider
+                key={item.id}
+                position={item.position}
+            />
+        }
+    }
+    )
+
   return (
     <div className="App">
-        <Header position="1">
-            <ActionButton text="up"/>
-            <ActionButton text="down" />
-        </Header>
-        <Slider position="2">
-            <ActionButton text="up"/>
-            <ActionButton text="down" />
-        </Slider>
+        {blocks}
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        id: 1,
+        type: "header",
+        data: {
+            title: 'Header title',
+        },
+        position: 1
+    },
+    {
+        id: 2,
+        type: "slider",
+        data: [
+            {
+                title: 'Slide 1',
+                content: 'Content 1',
+                image: '',
+            },
+            {
+                title: 'Slide 2',
+                content: 'Content 2',
+                image: '',
+            },
+        ],
+        position: 2,
+    },
+]
