c2628c5b4b9d2bb2125d40d3b04546cf5647c9b2
Krzysztof Ilnicki <pohlaniacz@gmail.com>
2024-03-16 22:30:10 +0100

    prapare for useState (I hope)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './styles/App.scss';
 import Header from "./components/editor/Header";
 import Slider from "./components/editor/Slider";
-import ActionButton from "./components/layout/ActionButton";
 import data from "./data"
 import EditBox from "./components/editor/EditBox";
 
 function App() {
+
+    data.sort((a,b) => a.position - b.position);
 
     const blocks = data.map(item => {
         if (item.type === "header") {
