c59b240484f5722d16d131b144427b3d662773a3
Krzysztof Ilnicki <pohlaniacz@gmail.com>
2024-03-20 21:54:56 +0100

    Fix moving blocks

--- src/App.js
+++ src/App.js
@@ -12,36 +12,35 @@ export default function App() {
     function moveBlock(e) {
         e.stopPropagation();
 
-        const id = e.target.closest('section').getAttribute('data-id');
+        const id = Number(e.target.closest('section').getAttribute('data-id'));
         const type = e.target.getAttribute('data-action');
 
-        setBlocks(prevBlocks => {
-            const newData = [...prevBlocks]; // Create a copy of the data array
+        const newData = JSON.parse(JSON.stringify(blocks));
 
-            const currentElementIndex = newData.findIndex(element => element.id === id);
-            const currentElement = newData[currentElementIndex];
+        const currentElementIndex = newData.findIndex(element => element.id === id);
+        const currentElement = newData[currentElementIndex];
 
-            if (type === "up") {
-                if (currentElementIndex > 0) {
-                    const previousElement = newData[currentElementIndex - 1];
+        if (type === "up") {
+            if (currentElementIndex > 0) {
+                const previousElement = newData[currentElementIndex - 1];
 
-                    [previousElement.position, currentElement.position] = [currentElement.position, previousElement.position];
-                    [newData[currentElementIndex - 1], newData[currentElementIndex]] = [currentElement, previousElement];
-                }
-            } else if (type === "down") {
-                if (currentElementIndex < newData.length - 1) {
-                    const nextElement = newData[currentElementIndex + 1];
-
-                    [nextElement.position, currentElement.position] = [currentElement.position, nextElement.position];
-                    [newData[currentElementIndex + 1], newData[currentElementIndex]] = [currentElement, nextElement];
-                }
+                [previousElement.position, currentElement.position] = [currentElement.position, previousElement.position];
+                [newData[currentElementIndex - 1], newData[currentElementIndex]] = [currentElement, previousElement];
             }
-            newData.sort((a,b) => a.position - b.position)
+        } else if (type === "down") {
+            if (currentElementIndex < newData.length - 1) {
+                const nextElement = newData[currentElementIndex + 1];
 
-            return newData;
-        });
+                [nextElement.position, currentElement.position] = [currentElement.position, nextElement.position];
+                [newData[currentElementIndex + 1], newData[currentElementIndex]] = [currentElement, nextElement];
+            }
+        }
+        newData.sort((a,b) => a.position - b.position)
 
+        setBlocks(newData);
     }
+
+
 
     return (
         <div className="App">
