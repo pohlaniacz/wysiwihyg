a3564e792c445620f7a2b069ce4b9ab9402afeeb
Krzysztof Ilnicki <pohlaniacz@gmail.com>
2024-05-14 23:01:28 +0200

    add default blocks

---
+++ src/components/defaults.js
@@ -0,0 +1,84 @@
+let header = {
+    "id": 1,
+    "type": "header",
+    "data": {
+        "firstLine": {
+            "text": "First line",
+            "font": {
+                "name": "Parisienne",
+                "size": 60
+            }
+        },
+        "secondLine": {
+            "text": "second line",
+            "font": {
+                "name": "Parisienne",
+                "size": 40
+            }
+        },
+        "image": ""
+    },
+    "position": 1
+};
+
+
+let twoColumns = {
+    "id": 33,
+    "type": "two-columns",
+    "data": {
+        "one": {
+            "firstLine": {
+                "text": "First line",
+                "font": {
+                    "name": "Parisienne",
+                    "size": 60
+                }
+            },
+            "secondLine": {
+                "text": "second line",
+                "font": {
+                    "name": "Parisienne",
+                    "size": 40
+                }
+            },
+            "paragraph": {
+                "text": "Lorem ipsum",
+                "font": {
+                    "name": "Parisienne",
+                    "size": 40
+                }
+            },
+            "image": {
+                "src": "",
+                "type": "right"
+            }
+        },
+        "two": {
+            "firstLine": {
+                "text": "First line",
+                "font": {
+                    "name": "Parisienne",
+                    "size": 60
+                }
+            },
+            "secondLine": {
+                "text": "second line",
+                "font": {
+                    "name": "Parisienne",
+                    "size": 40
+                }
+            },
+            "paragraph": {
+                "text": "Lorem ipsum",
+                "font": {
+                    "name": "Parisienne",
+                    "size": 40
+                }
+            },
+            "image": {
+                "src": "",
+                "type": "left"
+            }
+        }
+    }
+}
