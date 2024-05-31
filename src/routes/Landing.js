d7200db5ebd77ad02694a29e1bb45062097ca8b5
Krzysztof Ilnicki <pohlaniacz@gmail.com>
2024-05-31 23:41:16 +0200

    better landing

--- src/routes/Landing.js
+++ src/routes/Landing.js
@@ -1,11 +1,275 @@
 import Header from "../components/landing/Header";
 import MainSection from "../components/landing/MainSection";
+import { useState } from 'react'
+import { Dialog, DialogPanel } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid'
+
+
+const navigation = [
+    { name: 'Product', href: '#' },
+    { name: 'Features', href: '#' },
+    { name: 'Marketplace', href: '#' },
+    { name: 'Company', href: '#' },
+]
+
+const features = [
+    {
+        name: 'Push to deploy.',
+        description:
+            'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+        icon: CloudArrowUpIcon,
+    },
+    {
+        name: 'SSL certificates.',
+        description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
+        icon: LockClosedIcon,
+    },
+    {
+        name: 'Database backups.',
+        description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+        icon: ServerIcon,
+    },
+]
 
 export default function Landing() {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
     return (
-        <>
-            <Header />
-            <MainSection />
-        </>
+        <div className="bg-white">
+            <header className="absolute inset-x-0 top-0 z-50">
+                <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+                    <div className="flex lg:flex-1">
+                        <a href="#" className="-m-1.5 p-1.5">
+                            <span className="sr-only">Your Company</span>
+                            <img
+                                className="h-8 w-auto"
+                                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                                alt=""
+                            />
+                        </a>
+                    </div>
+                    <div className="flex lg:hidden">
+                        <button
+                            type="button"
+                            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                            onClick={() => setMobileMenuOpen(true)}
+                        >
+                            <span className="sr-only">Open main menu</span>
+                            <Bars3Icon className="h-6 w-6" aria-hidden="true"/>
+                        </button>
+                    </div>
+                    <div className="hidden lg:flex lg:gap-x-12">
+                        {navigation.map((item) => (
+                            <a key={item.name} href={item.href}
+                               className="text-sm font-semibold leading-6 text-gray-900">
+                                {item.name}
+                            </a>
+                        ))}
+                    </div>
+                    <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+                        <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                            Log in <span aria-hidden="true">&rarr;</span>
+                        </a>
+                    </div>
+                </nav>
+                <Dialog className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+                    <div className="fixed inset-0 z-50"/>
+                    <DialogPanel
+                        className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                        <div className="flex items-center justify-between">
+                            <a href="#" className="-m-1.5 p-1.5">
+                                <span className="sr-only">Your Company</span>
+                                <img
+                                    className="h-8 w-auto"
+                                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                                    alt=""
+                                />
+                            </a>
+                            <button
+                                type="button"
+                                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                                onClick={() => setMobileMenuOpen(false)}
+                            >
+                                <span className="sr-only">Close menu</span>
+                                <XMarkIcon className="h-6 w-6" aria-hidden="true"/>
+                            </button>
+                        </div>
+                        <div className="mt-6 flow-root">
+                            <div className="-my-6 divide-y divide-gray-500/10">
+                                <div className="space-y-2 py-6">
+                                    {navigation.map((item) => (
+                                        <a
+                                            key={item.name}
+                                            href={item.href}
+                                            className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                        >
+                                            {item.name}
+                                        </a>
+                                    ))}
+                                </div>
+                                <div className="py-6">
+                                    <a
+                                        href="#"
+                                        className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                    >
+                                        Log in
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </DialogPanel>
+                </Dialog>
+            </header>
+
+            <div className="relative isolate px-6 pt-14 lg:px-8">
+                <div
+                    className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+                    aria-hidden="true"
+                >
+                    <div
+                        className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+                        style={{
+                            clipPath:
+                                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                        }}
+                    />
+                </div>
+                <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+                    <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+                        <div
+                            className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                            Announcing our next round of funding.{' '}
+                            <a href="#" className="font-semibold text-indigo-600">
+                                <span className="absolute inset-0" aria-hidden="true"/>
+                                Read more <span aria-hidden="true">&rarr;</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="text-center">
+                        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                            Data to enrich your online business
+                        </h1>
+                        <p className="mt-6 text-lg leading-8 text-gray-600">
+                            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
+                            Elit sunt amet
+                            fugiat veniam occaecat fugiat aliqua.
+                        </p>
+                        <div className="mt-10 flex items-center justify-center gap-x-6">
+                            <a
+                                href="#"
+                                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                Get started
+                            </a>
+                            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                                Learn more <span aria-hidden="true">→</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div
+                    className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+                    aria-hidden="true"
+                >
+                    <div
+                        className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+                        style={{
+                            clipPath:
+                                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                        }}
+                    />
+                </div>
+            </div>
+            <div className="overflow-hidden bg-white py-24 sm:py-32">
+                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                    <div
+                        className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+                        <div className="lg:pr-8 lg:pt-4">
+                            <div className="lg:max-w-lg">
+                                <h2 className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
+                                <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">A better
+                                    workflow</p>
+                                <p className="mt-6 text-lg leading-8 text-gray-600">
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit
+                                    perferendis suscipit eaque,
+                                    iste dolor cupiditate blanditiis ratione.
+                                </p>
+                                <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
+                                    {features.map((feature) => (
+                                        <div key={feature.name} className="relative pl-9">
+                                            <dt className="inline font-semibold text-gray-900">
+                                                <feature.icon className="absolute left-1 top-1 h-5 w-5 text-indigo-600"
+                                                              aria-hidden="true"/>
+                                                {feature.name}
+                                            </dt>
+                                            {' '}
+                                            <dd className="inline">{feature.description}</dd>
+                                        </div>
+                                    ))}
+                                </dl>
+                            </div>
+                        </div>
+                        <img
+                            src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+                            alt="Product screenshot"
+                            className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+                            width={2432}
+                            height={1442}
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
+                <div
+                    className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+                    <svg
+                        viewBox="0 0 1024 1024"
+                        className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
+                        aria-hidden="true"
+                    >
+                        <circle cx={512} cy={512} r={512} fill="url(#759c1415-0410-454c-8f7c-9a820de03641)"
+                                fillOpacity="0.7"/>
+                        <defs>
+                            <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
+                                <stop stopColor="#7775D6"/>
+                                <stop offset={1} stopColor="#E935C1"/>
+                            </radialGradient>
+                        </defs>
+                    </svg>
+                    <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
+                        <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                            Boost your productivity.
+                            <br/>
+                            Start using our app today.
+                        </h2>
+                        <p className="mt-6 text-lg leading-8 text-gray-300">
+                            Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing
+                            sagittis vel nulla.
+                        </p>
+                        <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
+                            <a
+                                href="#"
+                                className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+                            >
+                                Get started
+                            </a>
+                            <a href="#" className="text-sm font-semibold leading-6 text-white">
+                                Learn more <span aria-hidden="true">→</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="relative mt-16 h-80 lg:mt-8">
+                        <img
+                            className="absolute left-0 top-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10"
+                            src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+                            alt="App screenshot"
+                            width={1824}
+                            height={1080}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
