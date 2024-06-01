369fe3119beb162b12c6ef156a9b1ecdaac4531a
Krzysztof Ilnicki <pohlaniacz@gmail.com>
2024-06-01 23:26:51 +0200

    netlify fix

--- src/routes/Landing.js
+++ src/routes/Landing.js
@@ -16,14 +16,14 @@ export default function Landing() {
             <header className="absolute inset-x-0 top-0 z-50">
                 <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
                     <div className="flex lg:flex-1">
-                        <a href="#" className="-m-1.5 p-1.5">
+                        <button className="-m-1.5 p-1.5">
                             <span className="sr-only">Your Company</span>
                             <img
                                 className="h-8 w-auto"
                                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                                 alt=""
                             />
-                        </a>
+                        </button>
                     </div>
                 </nav>
             </header>
@@ -50,15 +50,14 @@ export default function Landing() {
                             Created from awesome blocks.
                         </p>
                         <div className="mt-10 flex items-center justify-center gap-x-6">
-                            <a
-                                href="#"
+                            <button
                                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                             >
                                 Open existing
-                            </a>
-                            <a href="#" className="text-sm font-semibold leading-6 text-gray-900" onClick={redirectToUuidPage}>
+                            </button>
+                            <button className="text-sm font-semibold leading-6 text-gray-900" onClick={redirectToUuidPage}>
                                 Create <span aria-hidden="true">+</span>
-                            </a>
+                            </button>
                         </div>
                     </div>
                 </div>
