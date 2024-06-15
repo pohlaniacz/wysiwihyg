f48db556b67963794c3550305acf8e892bc3bb89
Krzysztof Ilnicki <pohlaniacz@gmail.com>
2024-06-15 23:10:59 +0200

    styles part 1

--- src/components/security/Signup.js
+++ src/components/security/Signup.js
@@ -34,47 +34,74 @@ const Signup = () => {
         <main >
             <section>
                 <div>
-                    <div>
-                        <h1> FocusApp </h1>
-                        <form>
-                            <div>
-                                <label htmlFor="email-address">
-                                    Email address
-                                </label>
-                                <input
-                                    type="email"
-                                    value={email}
-                                    onChange={(e) => setEmail(e.target.value)}
-                                    required
-                                    placeholder="Email address"
-                                />
-                            </div>
+                    <div className="relative isolate px-6 pt-14 lg:px-8">
+                        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+                            <form>
+                                <div className="space-y-12">
+                                    <div className="border-b border-gray-900/10 pb-12">
+                                        <h2 className="text-base font-semibold leading-7 text-gray-900">Profile</h2>
+                                        <p className="mt-1 text-sm leading-6 text-gray-600">
+                                            This information will be displayed publicly so be careful what you share.
+                                        </p>
 
-                            <div>
-                                <label htmlFor="password">
-                                    Password
-                                </label>
-                                <input
-                                    type="password"
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    required
-                                    placeholder="Password"
-                                />
-                            </div>
+                                        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                                            <div className="sm:col-span-4">
+                                                <label htmlFor="username"
+                                                       className="block text-sm font-medium leading-6 text-gray-900">
+                                                    Username
+                                                </label>
+                                                <div className="mt-2">
+                                                    <div
+                                                        className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                                        <input
+                                                            type="email"
+                                                            value={email}
+                                                            onChange={(e) => setEmail(e.target.value)}
+                                                            className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                                            required
+                                                            placeholder="Email address"
+                                                        />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
 
-                            <button
-                                type="submit"
-                                onClick={onSubmit}
-                            >
-                                Sign up
-                            </button>
+                                        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                                            <div className="sm:col-span-4">
+                                                <label htmlFor="username"
+                                                       className="block text-sm font-medium leading-6 text-gray-900">
+                                                    Password
+                                                </label>
+                                                <div className="mt-2">
+                                                    <div
+                                                        className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                                        <input
+                                                            type="password"
+                                                            value={password}
+                                                            onChange={(e) => setPassword(e.target.value)}
+                                                            className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                                            required
+                                                            placeholder="Password"
+                                                        />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
 
-                        </form>
+                                <button
+                                    type="submit"
+                                    onClick={onSubmit}
+                                >
+                                    Sign up
+                                </button>
+                            </form>
+                        </div>
 
                         <p>
                             Already have an account?{' '}
-                            <NavLink to="/s/login" >
+                            <NavLink to="/s/login">
                                 Sign in
                             </NavLink>
                         </p>
